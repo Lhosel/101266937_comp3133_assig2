dda9d486dfa3bff0e2ad805de73ed28cfa065f4a
Lhosel <kungalhosel20@gmail.com>
2022-04-07 19:19:16 -0400

    completed home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { SignupComponent } from './signup/signup.component';
@@ -7,6 +8,7 @@ import { ViewlistingsComponent } from './viewlistings/viewlistings.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'listings', component: ViewlistingsComponent },
