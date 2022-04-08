0340e096be25f4577705c46171be392ed61bdca1
Lhosel <kungalhosel20@gmail.com>
2022-04-08 13:43:33 -0400

    completed user booking history

--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-bookings',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingsComponent implements OnInit {
 
-  constructor() { }
+  bookings?: any;
+  username = window.localStorage.getItem("username");
+
+  private VIEW_USER_BOOKINGS = gql`
+    query UserBookings($username: String) {
+      userBookings(username: $username) {
+        id
+        listing_id
+        booking_id
+        booking_date
+        booking_start
+        booking_end
+        username
+      }
+    }
+  `
+
+  constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
+    this.getBookingsByUsername();
+  }
+
+  getBookingsByUsername() {
+    this.apollo.watchQuery<any>({
+      query: this.VIEW_USER_BOOKINGS,
+      variables: {
+        username: this.username
+      }
+    }).valueChanges.subscribe((res) => {
+      this.bookings = res.data.userBookings;
+
+      console.log(this.bookings);
+    });
   }
 
 }
